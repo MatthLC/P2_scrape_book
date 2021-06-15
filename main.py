66ed59b1705b40d77d8eef1a886bbe62b7bb1c64
MatthLC <gplayxz@gmail.com>
2021-06-15 12:59:21 +0200

    Scrape 1 livre : Optimisation du code

--- main.py
+++ main.py
@@ -3,61 +3,60 @@ from bs4 import BeautifulSoup
 import csv
 import os
 
-# Create directory for output:
-try:
-	os.mkdir('./output')
-except FileExistsError:
-	pass
-
-db = {	 "product_page_url":"product_page_url"
-		,"universal_product_code":"UPC"
-		,"title":"title"
-		,"price_including_tax":"Price (incl. tax)"
-		,"price_excluding_tax":"Price (excl. tax)"
-		,"number_available":"Availability"
-		,"product_description":""
-		,"category":""
-		,"review_rating":"Number of reviews"
-		,"image_url":"image_url"
+db = {	 'product_page_url':'product_page_url'
+		,'universal_product_code':'UPC'
+		,'title':'title'
+		,'price_including_tax':'Price (incl. tax)'
+		,'price_excluding_tax':'Price (excl. tax)'
+		,'number_available':'Availability'
+		,'product_description':''
+		,'category':''
+		,'review_rating':'Number of reviews'
+		,'image_url':'image_url'
 		}
 
-# "http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html"
+def create_dir(p_name):
+	# Create directory for output:
+	try:
+		os.mkdir('./' + p_name)
+	except FileExistsError:
+		pass
 
 def init_soup(p_url):
-	global soup
-
 	url = p_url
 	check_url = requests.get(url)
 
 	if check_url.ok:
 		soup = BeautifulSoup(check_url.text, 'html.parser')
+		return soup
 
-		book()
+
+def find_add_to_db(p_arg1, p_arg2, p_arg3, p_add_find):
+	if p_add_find != '':
+		f_var = soup.find(p_arg1, {p_arg2 : p_arg3}).find(p_add_find)
+	else:
+		f_var = soup.find(p_arg1, {p_arg2 : p_arg3})
+	return f_var
+
+def build_list(p_object):
+	product_info_trs = soup.findAll('tr')
+	
+	f_list = []
+	for obj in product_info_trs:
+		f_list.append(obj.find(p_object).text)
+
+	return f_list
 
 def book():	
 	#URL
-	db["product_page_url"] = url
+	db['product_page_url'] = url
 
-	#titre
-	title = soup.find('div', {'class':'col-sm-6 product_main'}).find('h1')
-	db["title"] = title.text
+	#title
+	db['title']=find_add_to_db('div', 'class', 'col-sm-6 product_main', 'h1').text
 
 	#product description
-	product_page = soup.find('article', {'class':'product_page'})
-	db['product_description'] = product_page.findAll('p')[3].text
-
-	#product information
-	product_info_trs = soup.findAll('tr')
-
-	trs_name = []
-	for trs in product_info_trs:
-		trs_name.append(trs.find('th').text)
-
-	tds_name = []
-	for tds in product_info_trs:
-		tds_name.append(tds.find('td').text)	
-
-	product_info = dict(zip(trs_name,tds_name))
+	db['product_description'] = find_add_to_db('article', 'class', 'product_page', '').findAll('p')[3].text
+	product_info = dict(zip(build_list('th'),build_list('td')))
 
 	for db_cle, db_name in db.items():
 		for info_cle, info_info in product_info.items():
@@ -65,26 +64,26 @@ def book():
 				db[db_cle] = info_info
 
 	#image url
-	img_url = soup.find('div',{'class':'item active'}).find('img')
+	db['image_url'] = 'http://books.toscrape.com/' + find_add_to_db('div', 'class', 'item active', 'img')['src'][6:]
 
-	db['image_url'] = 'http://books.toscrape.com/' + img_url['src'][6:]
-	
 	#category
-	category = soup.find('ul',{'class':'breadcrumb'}).text
-	lst_category = category.split()
-	db['category'] = lst_category[2]
+	db['category'] = find_add_to_db('ul','class','breadcrumb','').text.split()[2]
 		
 		
 def output_csv(p_name):
-	with open('./output/' + p_name + '.csv', 'a', encoding="utf-8") as create_csv:
+	with open('./output/' + p_name + '.csv', 'a', encoding='utf-8') as create_csv:
 		writer = csv.DictWriter(create_csv,db.keys())
-		# A faire : Warning plusieurs headers
+		#TODO: Multiple headers
 		writer.writeheader()
 		writer.writerow(db)
 
-if __name__ == "__main__":
+if __name__ == '__main__':
+	create_dir('output')
 
 	url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
-	init_soup(url)
 	
-	output_csv("livre")
+	soup = init_soup(url)
+
+	if soup != None:
+		book()
+		output_csv('livre')
